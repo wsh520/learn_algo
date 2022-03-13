afd32393e08677e7301cbfe75cd60fda71843a91
不如归去 <wl16675181281@163.com>
2022-03-13 10:57:45 +0800

    创建算法学习项目

---
+++ algo/src/test/java/com/wl/algo/AlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl.algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
